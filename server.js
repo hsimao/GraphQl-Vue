d5840d3c23c87580fa44a5f50a5a520cfa10eb25
hsimao <e087754958@gmail.com>
2018-09-30 16:57:30 +0800

    add typeDefs.gql

--- server.js
+++ server.js
@@ -1,33 +1,36 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer } = require("apollo-server");
 const mongoose = require("mongoose");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "typeDefs.gql");
+const typeDefs = fs.readFileSync(filePath, "utf-8");
 
 // 獲取資料庫URI
 require("dotenv").config({ path: "variables.env" });
+
+// 資料Schema
+const User = require("./models/User");
+const Post = require("./models/Post");
 
 // mongo DB連結
 mongoose
   .connect(
     process.env.MONGO_URI,
-    { useNewUrlParser: true }
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    }
   )
   .then(() => console.log("DB connected"))
   .catch(err => console.error(err));
 
-// 資料型別設置
-const typeDefs = gql`
-  type Todo {
-    task: String
-    completed: Boolean
-  }
-
-  # 查詢
-  type Query {
-    getTodos: [Todo]
-  }
-`;
-
 const server = new ApolloServer({
-  typeDefs
+  typeDefs,
+  context: {
+    User,
+    Post
+  }
 });
 
 server.listen().then(({ url }) => {

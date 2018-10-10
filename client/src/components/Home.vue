6226da9ac52ad17ebd41995223f3bcf12589eb55
hsimao <e087754958@gmail.com>
2018-10-10 20:27:19 +0800

    add: client Apollo setting, getPost data, 設置前端Apollo, 取得getPost資料

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,16 +1,32 @@
 <template>
   <v-container>
     <h1>Home</h1>
+    <ul v-for="(post, index) in getPosts" :key="index">
+      <li>{{post.title}}</li>
+      <li>{{post.description}}</li>
+      <li>{{post.likes}}</li>
+    </ul>
   </v-container>
 </template>
 
 <script>
+import { gql } from "apollo-boost";
+
 export default {
   name: "Home",
-  data() {
-    return {
-      add: ""
-    };
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `
+    }
   }
 };
 </script>
--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,27 @@
-import '@babel/polyfill'
+import "@babel/polyfill";
 import Vue from "vue";
-import './plugins/vuetify'
+import "./plugins/vuetify";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 引入 Apollo
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+
+Vue.use(VueApollo);
+
+// 設置Apollo
+const defaultClient = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
+
+const apolloProvider = new VueApollo({ defaultClient });
+
 Vue.config.productionTip = false;
 
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   render: h => h(App)

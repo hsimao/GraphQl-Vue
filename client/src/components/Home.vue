5f2a63c06e7a397deb0f4be55d79f96f71ed89ac
hsimao <e087754958@gmail.com>
2018-10-10 20:51:39 +0800

    add: smart Apollo query, Apollo 其他應用

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <v-container>
+
     <h1>Home</h1>
-    <ul v-for="(post, index) in getPosts" :key="index">
+    <h2 v-if="$apollo.loading">Loading...</h2>
+    <ul v-else v-for="(post, index) in getPosts" :key="index">
       <li>{{post.title}}</li>
       <li>{{post.description}}</li>
       <li>{{post.likes}}</li>
@@ -14,6 +16,11 @@ import { gql } from "apollo-boost";
 
 export default {
   name: "Home",
+  data() {
+    return {
+      posts: []
+    };
+  },
   apollo: {
     getPosts: {
       query: gql`
@@ -25,7 +32,17 @@ export default {
             likes
           }
         }
-      `
+      `,
+      // 回傳資訊，可回傳loading, data, 如要將資料傳到其他地方，例如vue 的data,可以在此操作
+      result(args) {
+        console.dir(args);
+        if (!args.loading) {
+          this.posts = args.data.getPosts;
+        }
+      },
+      error(err) {
+        console.log("Error", err);
+      }
     }
   }
 };

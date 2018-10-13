6b84f1716bc6c06aefb8e56bc0489deac9864573
hsimao <e087754958@gmail.com>
2018-10-13 17:50:49 +0800

    add: client setting token in headers authorization to backend, 新增token到請求的header授權上，來送到後端

--- client/src/main.js
+++ client/src/main.js
@@ -13,7 +13,34 @@ Vue.use(VueApollo);
 
 // 設置Apollo
 export const defaultClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: "http://localhost:4000/graphql",
+  // 將token跟請求送到後端
+  fetchOptions: {
+    credentials: "include"
+  },
+  request: operation => {
+    // 如果localStorage沒有token, 將給空白字串, 預防報錯
+    if (!localStorage.token) {
+      localStorage.setItem("token", "");
+    }
+
+    // 將token添加到header來取得授權
+    operation.setContext({
+      headers: {
+        authorization: localStorage.getItem("token")
+      }
+    });
+  },
+  onError: ({ graphQLErrors, networkError }) => {
+    if (networkError) {
+      console.log("[網路異常]", networkError);
+    }
+    if (graphQLErrors) {
+      for (let err of graphQLErrors) {
+        console.dir(err);
+      }
+    }
+  }
 });
 
 const apolloProvider = new VueApollo({ defaultClient });

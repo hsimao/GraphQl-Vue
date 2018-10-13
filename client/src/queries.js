d10834bcc5bd6ac839155a2db196a72a99b2f0e4
hsimao <e087754958@gmail.com>
2018-10-13 13:53:11 +0800

    add: client Apollo mutation signin、signup, 前端Apollo新增登入、註冊方法

--- client/src/queries.js
+++ client/src/queries.js
@@ -10,3 +10,21 @@ export const GET_POSTS = gql`
     }
   }
 `;
+
+// 用戶登入
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+
+// 用戶註冊
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
+    }
+  }
+`;
